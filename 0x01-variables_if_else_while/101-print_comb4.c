4895a81fc74a8f65a835ea9db117322b2767d762
Timothy Elelewor <timothyelelewor@gmail.com>
2022-10-20 22:23:37 -0700

    task to combine three different digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			for (k = '2'; k <= '9'; k++)
+			{
+				if (i == j || i == k || j == k || i > j || i > k || j > k)
+					continue;
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+					if (i == '7' && j == '8' && k == '9')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
