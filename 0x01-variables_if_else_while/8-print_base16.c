c705059e311fbe4f4a63d0266564bc33ce7287f0
Timothy Elelewor <timothyelelewor@gmail.com>
2022-10-20 21:57:13 -0700

    task to print hexadimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int ch;
+
+	for (i = '0'; i <= '9'; i++)
+
+		putchar(i);
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
