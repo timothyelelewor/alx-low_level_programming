15003fba661980955e43bb9ab7a9cbb4e940b6e4
Timothy Elelewor <timothyelelewor@gmail.com>
2022-10-25 05:59:06 -0700

    pointer to change an int value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	int j = *b;
+
+	*a = j;
+	*b = i;
+}
